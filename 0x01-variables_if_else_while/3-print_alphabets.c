28818aaa4574fe43c89f687f596dbfa69de73263
Etim Esin <113884605+smish2@users.noreply.github.com>
2022-10-18 01:44:46 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry Point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		char a;
-			char A;
+	char a;
+	char A;
 
-				for (a = 'a'; a <= 'z'; a++)
-							putchar(a);
-					for (A = 'A'; A <= 'Z'; A++)
-								putchar(A);
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (A = 'A'; A <= 'Z'; A++)
+		putchar(A);
 
-						putchar('\n');
+	putchar('\n');
 
-							return (0);
+	return (0);
 }
