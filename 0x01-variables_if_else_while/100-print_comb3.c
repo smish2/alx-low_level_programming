73cb0e01d0234008c4069efc55c45f9d7cee6e58
Etim Esin <113884605+smish2@users.noreply.github.com>
2022-10-18 02:00:07 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,57 +1,29 @@
-nclude <stdio.h>
-
-
+#include <stdio.h>
 
 /**
+ * main - Entry Point
  *
- *  * main - Entry Point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	int digit1, digit2;
 
-		int digit1, digit2;
+	for (digit1 = 0; digit1 <= 9; digit1++)
+		for (digit2 = 0; digit2 <= 9; digit2++)
+		{
+			if (digit1 == digit2 || digit2 < digit1)
+				continue;
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			if (digit1 == 8 && digit2 == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
 
+	putchar('\n');
 
-
-			for (digit1 = 0; digit1 <= 9; digit1++)
-
-						for (digit2 = 0; digit2 <= 9; digit2++)
-
-									{
-
-													if (digit1 == digit2 || digit2 < digit1)
-
-																		continue;
-
-																putchar(digit1 + '0');
-
-																			putchar(digit2 + '0');
-
-																						if (digit1 == 8 && digit2 == 9)
-
-																											break;
-
-																									putchar(',');
-
-																												putchar(' ');
-
-																														}
-
-
-
-				putchar('\n');
-
-
-
-					return (0);
-
+	return (0);
 }
