996f029da2455f535f87b66a736d5ef7b5d826a6
Etim Esin <113884605+smish2@users.noreply.github.com>
2022-10-19 18:08:13 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * _isalpha - Checks for alphabetic character
-  * @c: The character to be checked
-  *
-  * Return: 1 for alphabetic character or 0 for anything else
+* _isalpha - Checks for alphabetic character
+* @c: The character to be checked
+*
+* Return: 1 for alphabetic character or 0 for anything else
   */
 int _isalpha(int c)
 {
