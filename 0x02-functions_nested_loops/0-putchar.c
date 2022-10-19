6cd64df4ece3cf0442bda728722a0a79d6e3fc54
smish2 <eetunde@gmail.com>
2022-10-19 01:24:29 -0700

    put char and read me

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "Putchar print"
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always returns 0
+ *
+ *     *
+ *
+ *      */
+int main(void)
+{
+ _putchar('_putchar');
+ _putchar('\n');
+ return (0);
+}
